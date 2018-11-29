f60c9f2c8d3ca3e1ea1a427941f55039828304cd
Inani El Houssain <inanielhoussain@gmail.com>
2018-11-29 16:07:19 +0000

    Model/events/communication component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular101';
+
+    linkClicked(){
+      console.log('Link Clicked')
+    }
 }
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,9 +1,14 @@
-import {Component} from '@angular/core';
-
+import {Component, Input, Output, EventEmitter} from '@angular/core';
 @Component({
     selector : 'app-header',
     templateUrl: './header.component.html'
 })
 export class HeaderComponent {
-
+    url = 'https://www.youtube.com/watch?v=AaNZBrP26LQ';
+    @Input('label') label: string = 'sdf';
+    @Output() link: EventEmitter<any> = new EventEmitter();
+    clickMe(event){
+        event.preventDefault();
+        this.link.emit()
+    }
 }
